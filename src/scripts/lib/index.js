2eda1723ad59c09f508472a60faa53f987dd82f6
Vladyslav Korobka <vladseipa@gmail.com>
2024-10-07 17:25:52 +0300

    resolved problem with scroll

--- src/scripts/lib/index.js
+++ src/scripts/lib/index.js
@@ -1,1 +1,10 @@
-'use strict';
+const showNav = document.querySelector('a[href="#top-menu"]');
+const closeNav = document.querySelector('a[href="#home"]');
+const navItem = document.querySelectorAll(".nav__item");
+
+const showScroll = () => document.body.classList.remove("no-scroll");
+const hiddenScroll = () => document.body.classList.add("no-scroll");
+
+showNav.addEventListener("click", hiddenScroll);
+closeNav.addEventListener("click", showScroll);
+navItem.forEach((item) => item.addEventListener("click", showScroll));
